5116ad91be70bd266f4252395d38fa3e0e313409
joshua1996 <kjw1996@hotmail.com>
2017-10-19 13:24:35 +0800

    first commit

---
+++ app/src/main/java/com/indieDream/wordChain/MainActivity.java
@@ -0,0 +1,67 @@
+package com.indieDream.wordChain;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView messageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Demo");
+        setSupportActionBar(toolbar);
+
+        ViewGroup tab = (ViewGroup) findViewById(R.id.tab);
+        tab.addView(LayoutInflater.from(this).inflate(R.layout.demo_basic, tab, false));
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+
+        FragmentPagerItems pages = new FragmentPagerItems(this);
+        pages.add(FragmentPagerItem.of("我参与的", DemoFragment.class));
+        pages.add(FragmentPagerItem.of("我围观的", DemoFragment.class));
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), pages);
+
+        viewPager.setAdapter(adapter);
+        viewPagerTab.setViewPager(viewPager);
+
+
+//
+//        messageView = (TextView) findViewById(R.id.messageView);
+//
+//        final BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+//        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+//            @Override
+//            public void onTabSelected(@IdRes int tabId) {
+//                // messageView.setText(TabMessage.get(tabId, false));
+//            }
+//        });
+//
+//        bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
+//            @Override
+//            public void onTabReSelected(@IdRes int tabId) {
+//                //  Toast.makeText(getApplicationContext(), TabMessage.get(tabId, true), Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+////        BottomBarTab nearby = bottomBar.getTabWithId(R.id.tab);
+//        //   nearby.setBadgeCount(5);
+    }
+}
